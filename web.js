0fdfd0bac3a082324293c3e96ea3975ae748ddcf
priyanshu706 <125879478+priyanshu706@users.noreply.github.com>
2024-07-17 23:07:50 +0530

    Update web.js

--- web.js
+++ web.js
@@ -1,1 +1,11 @@
+const express = require('express');
+const app = express()
+const port = 9433
 
+app.get('/',(req , res)=>{
+  res.send("Hello World It's my fake app to create  my own smtp server");
+})
+
+app.listen(port , (req,res)=>{
+  console.log(`YOur fake app is listning on port:${port}`);
+})
